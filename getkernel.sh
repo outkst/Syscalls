95aadfc37df5ff1b327377a770e2f1f3fa0b6b45
Joe Meszar <github@outk.st>
2016-10-04 19:19:13 -0400

    Updated verbose output for getkernel.sh
    The 'test' binary file is being grabbed, not 'test.c'

--- getkernel.sh
+++ getkernel.sh
@@ -1,6 +1,6 @@
 echo -e "Grabbing kernel 'bzImage' from thoth.cs.pitt.edu ..."
 echo -e "Grabbing system call map 'System.map' from thoth.cs.pitt.edu ..."
-echo -e "Grabbing test file 'test.c' from thoth.cs.pitt.edu ...\n"
+echo -e "Grabbing test file 'test' from thoth.cs.pitt.edu ...\n"
 
 scp jwm54@thoth.cs.pitt.edu:/afs/pitt.edu/home/j/w/jwm54/private/cs1550/project2//\{test,linux-2.6.23.1/arch/i386/boot/bzImage,linux-2.6.23.1/System.map\} .
 
