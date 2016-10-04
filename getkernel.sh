6a01c259d73e102fd64c7ebee81001ccb35d4364
Joe Meszar <github@outk.st>
2016-10-04 19:36:51 -0400

    Updated getkernel.sh to get 'prodcons' file instead of 'test'

--- getkernel.sh
+++ getkernel.sh
@@ -1,8 +1,8 @@
 echo -e "Grabbing kernel 'bzImage' from thoth.cs.pitt.edu ..."
 echo -e "Grabbing system call map 'System.map' from thoth.cs.pitt.edu ..."
-echo -e "Grabbing test file 'test' from thoth.cs.pitt.edu ...\n"
+echo -e "Grabbing file 'prodcons' from thoth.cs.pitt.edu ...\n"
 
-scp jwm54@thoth.cs.pitt.edu:/afs/pitt.edu/home/j/w/jwm54/private/cs1550/project2//\{test,linux-2.6.23.1/arch/i386/boot/bzImage,linux-2.6.23.1/System.map\} .
+scp jwm54@thoth.cs.pitt.edu:/afs/pitt.edu/home/j/w/jwm54/private/cs1550/project2//\{prodcons,linux-2.6.23.1/arch/i386/boot/bzImage,linux-2.6.23.1/System.map\} .
 
 echo -e "\nCopying 'bzImage' to /boot/bzImage-devel"
 echo -e "Copying 'System.map' to /boot/System.map-devel\n"
