1f8c48be49d1463efa8f637a566e480398a21471
Joe Meszar <mesz@outk.st>
2016-10-17 13:28:29 -0400

    Formatted comments in prodcons.h

--- src/prodcons.h
+++ src/prodcons.h
@@ -1,7 +1,11 @@
 /* 
- * Skeleton code for prodcons implementation
- * (C) Mohammad H. Mofrad, 2016  
- *
+	Syscalls (Producer/Consumer problem)
+	
+	Joe Meszar (jwm54@pitt.edu)
+	CS1550 Project 1 (FALL 2016)
+	
+	Skeleton code for prodcons implementation
+	(C) Mohammad H. Mofrad, 2016  
  */
 #ifndef PRODCONS_H_INCLUDED
 #define PRODCONS_H_INCLUDED
@@ -15,8 +19,8 @@
 	Custom linked-list implementation to hold list of processes
 */
 struct proc_node {
-	struct task_struct *process;		// holds a reference to a process
-	struct proc_node *next;				// holds reference to next process node in linked list
+	struct task_struct *process;	/* holds a reference to a process */
+	struct proc_node *next;		/* holds reference to next process node in linked list */
 };
 
 /* 
@@ -24,9 +28,9 @@ struct proc_node {
 	queue of processes waiting to use that resource
 */
 struct cs1550_sem {
-	int value;							/* holds available */
-	struct proc_node *head;				/* start of linked-list of processes waiting */
-	struct proc_node *tail;				/* end of linked-list of processes waiting */
+	int value;			/* holds available */
+	struct proc_node *head;		/* start of linked-list of processes waiting */
+	struct proc_node *tail;		/* end of linked-list of processes waiting */
 };
 
 void  cs1550_down(struct cs1550_sem *);
